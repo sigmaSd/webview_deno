80de36497d4043b0c4c34697b84c5953b1a76dfe
Nbiba Bedis <bedisnbiba@gmail.com>
2025-01-26 18:11:49 +0100

    temp fork

--- src/ffi.ts
+++ src/ffi.ts
@@ -1,12 +1,12 @@
-// import manifest from "../deno.json" with { type: "json" };
+import manifest from "../deno.json" with { type: "json" };
 
 import { dlopen, download } from "@denosaurs/plug";
 import type { Webview } from "./webview.ts";
 
-// const version = manifest.version;
+const version = manifest.version;
 const cache = Deno.env.get("PLUGIN_URL") === undefined ? "use" : "reloadAll";
 const url = Deno.env.get("PLUGIN_URL") ??
-  `https://github.com/sigmaSd/webview_deno/releases/download/untagged-600e42e8b91d05f3559e/`;
+  `https://github.com/sigmaSd/webview_deno/releases/download/${version}/`;
 
 const encoder = new TextEncoder();
 
